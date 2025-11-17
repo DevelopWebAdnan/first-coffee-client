2a45da9c5e732f6ce13c9b4022db58d737a088e5
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2025-11-17 13:07:22 +0600

    Deleta a coffee and fetch coffee for updating.

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
 
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Coffee has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div className="card card-side bg-base-100 shadow-sm">
@@ -16,13 +48,17 @@ const CoffeeCard = ({ coffee }) => {
                     <h2 className="card-title">Name: {name}</h2>
                     <p>{quantity}</p>
                     <p>{supplier}</p>
-                    <p>{category}</p>
+                    <p>{taste}</p>
                 </div>
                 <div className="card-actions">
                     <div className="join join-vertical space-y-4">
                         <button className="btn join-item">View</button>
-                        <button className="btn join-item">Edit</button>
-                        <button className="btn join-item">X</button>
+                        <Link to={`/update/${_id}`}>
+                            <button className="btn join-item">Edit</button>
+                        </Link>
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn join-item">X</button>
                     </div>
                 </div>
             </div>
